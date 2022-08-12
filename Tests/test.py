7ed652329aeeca1adbda5c6d2ed354c4f130fcf0
Puja-Adak <111103095+Puja-Adak@users.noreply.github.com>
2022-08-12 14:05:08 +0800

    Update test.py

--- Tests/test.py
+++ Tests/test.py
@@ -1,14 +1,19 @@
-import S3Decoder as S3Decoder
+# importing sys
+import sys
+ # adding Folder_2 to the system path
+sys.path.insert(0, 'C:\Github\S3_URL_DECODER_PYTHON\Source')
+
+from S3Decoder import s3decode
 
 #S3 bucket structure {my_bucket_name}\folder name \filename
 
 # key is "folder name \filename" to download the S3 file 
 
-
+ 
 #test case 1
 
 S3_keyname = "input\Z00010246+%281%29.PNG"
-print(S3Decoder.s3decode(S3_keyname))
+print(s3decode(S3_keyname))
 # output 
 #input\Z00010246 (1).PNG
 
@@ -16,18 +21,18 @@ print(S3Decoder.s3decode(S3_keyname))
 #test case 2
 
 S3_keyname = "input\Z00010246 %281%29.PNG"
-print(S3Decoder.s3decode(S3_keyname))
+print(s3decode(S3_keyname))
 #output = input\Z00010246 (1).PNG
 
 #test case 3
 
 S3_keyname = "input\Z00010246.png"
-print(S3Decoder.s3decode(S3_keyname))
+print(s3decode(S3_keyname))
 #output = input\Z00010246.png
 
 
 #test case 4
 
 S3_keyname = "input\Z00010246 %28-1%29.png"
-print(S3Decoder.s3decode(S3_keyname))
+print(s3decode(S3_keyname))
 #output = input\Z00010246 (-1).png
